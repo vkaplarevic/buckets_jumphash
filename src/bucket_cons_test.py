c7a179c7fd4608bcb8eb7ac753bd3374e3f945ad
vkaplarevic <vlad.kaplarevic@gmail.com>
2018-05-01 23:45:20 +0200

    typo and white space

--- src/bucket_cons_test.py
+++ src/bucket_cons_test.py
@@ -20,58 +20,58 @@ def jump_hash(key, buckets):
     return b
 
 def shard_and_bucket(key, bnum, use_jump):
-	if use_jump:
-		return key % 16, (key % 16) * bnum + jump_hash(key, bnum)
+    if use_jump:
+        return key % 16, (key % 16) * bnum + jump_hash(key, bnum)
 
-	# Return my peasant hash!
-	return key % 16, (key % 16) * bnum + (((key + 51) // 10) * 31) % bnum
+    # Return my peasant hash!
+    return key % 16, (key % 16) * bnum + (((key + 51) // 10) * 31) % bnum
 
 
 def main():
-	if len(sys.argv) != 2:
-		return
+    if len(sys.argv) != 2:
+        return
 
-	if sys.argv[1] != 'jump' and sys.argv[1] != 'other':
-		print("Command line option can be either: jump or peasant")
-		return
+    if sys.argv[1] != 'jump' and sys.argv[1] != 'other':
+        print("Command line option can be either: jump or other")
+        return
 
-	use_jump = sys.argv[1] == 'jump'
+    use_jump = sys.argv[1] == 'jump'
 
-	shards = { x: {} for x in range(0,16) }
-	for x in range(0, 16):	
-		shards[x] = {}
-		for b in range(0, 6):
-			shards[x][x*6 + b] = 0
+    shards = { x: {} for x in range(0,16) }
+    for x in range(0, 16):    
+        shards[x] = {}
+        for b in range(0, 6):
+            shards[x][x*6 + b] = 0
 
-	# So, after this, each shard contains 6 buckets
-	# and every single bucket has unique tag...
-	miss = 0
-	for k in range(0, COUNT):
-		if r.randint(0, 9) == 1:
-			miss += 1
-			continue
+    # So, after this, each shard contains 6 buckets
+    # and every single bucket has unique tag...
+    miss = 0
+    for k in range(0, COUNT):
+        if r.randint(0, 9) == 1:
+            miss += 1
+            continue
 
-		shard, bucket = shard_and_bucket(k, 6, use_jump)
-		shards[shard][bucket] += 1
+        shard, bucket = shard_and_bucket(k, 6, use_jump)
+        shards[shard][bucket] += 1
 
-	print("miss:", miss, "\n")
+    print("miss:", miss, "\n")
 
-	# Let's print this. All buckets should have
-	# roughly the same size with the jump hash function.
-	bucket_values = []
-	for s in range(0, 16):
-		print("shard:", s)
-		for b in shards[s].keys():
-			print("    b" +  str(b) + ":" , shards[s][b])
-			bucket_values.append(shards[s][b])	
+    # Let's print this. All buckets should have
+    # roughly the same size with the jump hash function.
+    bucket_values = []
+    for s in range(0, 16):
+        print("shard:", s)
+        for b in shards[s].keys():
+            print("    b" +  str(b) + ":" , shards[s][b])
+            bucket_values.append(shards[s][b])    
 
-	bucket_values.sort()
+    bucket_values.sort()
 
-	print("\nmax:", max(bucket_values))
-	print("min:", min(bucket_values))
-	print("mean:", np.mean(bucket_values))
-	print("median:", (bucket_values[48] + bucket_values[47]) / 2)
-	print("std. dev:", np.std(bucket_values))
+    print("\nmax:", max(bucket_values))
+    print("min:", min(bucket_values))
+    print("mean:", np.mean(bucket_values))
+    print("median:", (bucket_values[48] + bucket_values[47]) / 2)
+    print("std. dev:", np.std(bucket_values))
 
 
 main()
